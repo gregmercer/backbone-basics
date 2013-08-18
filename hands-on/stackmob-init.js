930963618bb2d33c225510e5f4699377c602782f
Greg Mercer (GBear) <gmercer@stanford.edu>
2013-08-18 16:16:48 -0700

    added new other-examples files

--- hands-on/stackmob-init.js
+++ hands-on/stackmob-init.js
@@ -1,4 +1,12 @@
+
 StackMob.init({
     publicKey: "f8b77aff-8535-4b27-9b46-4d0253a6ce92",
     apiVersion: 0
 });
+
+/*
+StackMob.init({
+    publicKey: "0acbe70d-f3b3-49d8-b3e1-f1a6a4f8c43b",
+    apiVersion: 0
+});
+*/
